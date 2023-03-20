d163663291bd0dccda67a25e966db22f20fd1d88
ASAPJulio <juliosarafyan@gmail.com>
2023-03-20 11:42:30 +0100

    fixer nav scroll

---
+++ assets/js/nav.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.basNav');
+const navOffsetTop = nav.offsetTop;
+
+window.addEventListener('scroll', () => {
+
+        if(window.scrollY >= navOffsetTop) {
+            nav.style.position = 'fixed';
+            nav.style.top = 0;
+            nav.style.width = "100%";
+            
+        }
+        else {
+            nav.style.position = 'static';
+        }
+})
