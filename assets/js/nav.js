13670f497870cfd18addd16e4d49d44c6afa7717
ASAPJulio <juliosarafyan@gmail.com>
2023-03-20 13:49:44 +0100

    ajout transition nav scroll

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,13 +3,12 @@ const navOffsetTop = nav.offsetTop;
 
 window.addEventListener('scroll', () => {
 
-        if(window.scrollY >= navOffsetTop) {
-            nav.style.position = 'fixed';
-            nav.style.top = 0;
-            nav.style.width = "100%";
-            
-        }
-        else {
-            nav.style.position = 'static';
-        }
+    if (window.scrollY >= navOffsetTop) {
+        nav.style.position = 'fixed';
+        nav.style.top = 0;
+        nav.style.width = "100%";
+    }
+    else {
+        nav.style.position = 'static';
+    }
 })
