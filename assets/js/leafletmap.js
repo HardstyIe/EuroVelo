6c0f62d5869f37749d7cbd0427b2e9ae78131d87
Hardstyle <Hardstyle.dd@gmail.com>
2023-03-21 08:06:57 +0100

    ajout slider + debut map

---
+++ assets/js/leafletmap.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([50.967137, 1.849985], 13)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map)
