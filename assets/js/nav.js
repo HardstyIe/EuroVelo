3e53e805a91c9926e4d38b9fb8d5378d125f7d98
ASAPJulio <juliosarafyan@gmail.com>
2023-03-22 16:16:30 +0100

    js barre recherche

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,3 +1,6 @@
+////////////////////////////Js pour que la partie haute ne suive pas la partie basse de la nav en scrollant///////////////////////////////////////////
+
+
 const nav = document.querySelector('.basNav');
 const navOffsetTop = nav.offsetTop;
 
@@ -12,16 +15,23 @@ window.addEventListener('scroll', () => {
         nav.style.position = 'static';
     }
 })
-////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////Bouton de recherche de la barre de nav///////////////////////////////////
 
 
 const searchIcon = document.querySelector('.ti-search');
 const searchContainer = document.querySelector('.search-container');
 
+
 searchIcon.addEventListener('click', function() {
   if (searchContainer.style.display === 'none') {
     searchContainer.style.display = 'flex';
+    searchIcon.classList.remove('ti-search');
+    searchIcon.classList.add('ti-close');
   } else {
     searchContainer.style.display = 'none';
+    searchIcon.classList.add('ti-search');
+    searchIcon.classList.remove('ti-close');
   }
+
 });
