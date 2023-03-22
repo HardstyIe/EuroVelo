bddae9cbe9db4b509b91bf934f5a7dfaffd795cf
duarte-raphael <rafael.duarte@outlook.fr>
2023-03-22 09:38:23 +0100

    cree json et script avis

--- assets/js/avis.js
+++ assets/js/avis.js
@@ -1,28 +1,28 @@
-const reponse = await fetch('script.json');
-const avis = await reponse.json();
+// const reponse = await fetch('scriptAvis.json');
+// const avis = await reponse.json();
 
-for (let i = 0; i < avis.length; i++) {
+// for (let i = 0; i < avis.length; i++) {
 
-    const article = avis[i];
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".avisHtml");
-    // Création d’une balise dédiée à une pièce automobile
-    const avisElement = document.createElement("article");
-    // Création des balises 
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = article.nom;
-    const villeElement = document.createElement("p");
-    villeElement.innerText = article.ville;
-    const etoileElement = document.createElement("p");
-    etoileElement.innerText = article.etoile;
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description;
+//     const article = avis[i];
+//     // Récupération de l'élément du DOM qui accueillera les fiches
+//     const sectionFiches = document.querySelector(".avisHtml");
+//     // Création d’une balise dédiée à une pièce automobile
+//     const avisElement = document.createElement("article");
+//     // Création des balises 
+//     const nomElement = document.createElement("h2");
+//     nomElement.innerText = article.nom;
+//     const villeElement = document.createElement("p");
+//     villeElement.innerText = article.ville;
+//     const etoileElement = document.createElement("p");
+//     etoileElement.innerText = article.etoile;
+//     const descriptionElement = document.createElement("p");
+//     descriptionElement.innerText = article.description;
 
     
-    // On rattache la balise article a la section Fiches
-    sectionFiches.appendChild(avisElement);
-    avisElement.appendChild(nomElement);
-    avisElement.appendChild(villeElement);
-    avisElement.appendChild(etoileElement);
-    avisElement.appendChild(descriptionElement);
- }
+//     // On rattache la balise article a la section Fiches
+//     sectionFiches.appendChild(avisElement);
+//     avisElement.appendChild(nomElement);
+//     avisElement.appendChild(villeElement);
+//     avisElement.appendChild(etoileElement);
+//     avisElement.appendChild(descriptionElement);
+//  }
