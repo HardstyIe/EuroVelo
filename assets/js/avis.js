ed7d893bd113daa50e19e65573b715339934579f
HardstyIe <hardstyle.dd@gmail.com>
2023-03-26 23:12:08 +0200

    fix calcul avis + format date

--- assets/js/avis.js
+++ assets/js/avis.js
@@ -1,6 +1,6 @@
-let promise = fetch('http://90.110.218.245:5003/api/avis')
+fetch('http://90.110.218.245:5003/api/avis')
   .then(response => response.json())
-  .then(data => {
+  .then(async data => {
     let avis = data.data
     for (let i = 0; i < avis.length; i++) {
       const {
@@ -17,24 +17,62 @@ let promise = fetch('http://90.110.218.245:5003/api/avis')
         Lire
       } = avis[i].attributes
 
+      const date = new Date(Dates)
+      const dateFormatted = new Intl.DateTimeFormat('fr-FR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+      }).format(date)
+
       const enfant = `
-  <article>
-    <p class="avisDate">Date de Publication: ${Dates}</p>
-    <h2 class="avisNom">Nom: ${Nom}</h2>
-    <p class="avisType">Itinéraire du Parcours: ${Course}</p>
-    <div class="notesAvis">
+      <article>
+      <p class="avisDate">Date de Publication: ${dateFormatted}</p>
+      <h2 class="avisNom">Nom: ${Nom}</h2>
+      <p class="avisType">Itinéraire du Parcours: ${Course}</p>
+      <div class="notesAvis">
       <p>${Securite}</p>
       <p>${Balisage}</p>
       <p>${Interets}</p>
       <p>${Services}</p>
-    </div>
-    <p class="avisParcours">Type de Parcours: ${Parcours}</p>
-    <p class="avisComment">${Commentaire}</p>
-    <p class="nombreReponse">${Nbreponse}</p>
-    <p class="boutonAvis">${Lire}</p>
-  </article>
-  `
-
+      <img src=${await rate([
+        Securite,
+        Balisage,
+        Interets,
+        Services
+      ])} class="avisRating"/>
+      </div>
+      <p class="avisParcours">Type de Parcours: ${Parcours}</p>
+      <p class="avisComment">${Commentaire}</p>
+      <p class="nombreReponse">${Nbreponse}</p>
+      <p class="boutonAvis">${Lire}</p>
+      </article>
+      `
       document.querySelector('.avisHtml').innerHTML += enfant
     }
   })
+
+function numAverage(a) {
+  let b = a.length,
+    c = 0,
+    i = 0
+  for (i = 0; i < b; i++) {
+    c += parseInt(a[i])
+  }
+  return c / b
+}
+
+async function rate(arr) {
+  const response = await fetch(
+    `http://90.110.218.245:5003/api/stars/${Math.trunc(
+      numAverage(arr) * 2 + 1
+    )}`
+  )
+
+  const data = await response.json()
+
+  const rating = data.data.attributes.url
+
+  return 'http://90.110.218.245:5003' + rating
+}
