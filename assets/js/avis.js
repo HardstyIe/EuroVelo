96c176d3353c2c96d074478fca50a6a31698192c
duarte-raphael <rafael.duarte@outlook.fr>
2023-03-27 11:34:26 +0200

    la page contact est terminer avec son responsive

--- assets/js/avis.js
+++ assets/js/avis.js
@@ -1,42 +1,62 @@
-const reponse = await fetch('scriptAvis.json');
-const avis = await reponse.json();
 
-for (let i = 0; i < avis.length; i++) {
+fetch('scriptAvis.json') 
+    .then(res=>console.log(res))
 
-    const article = avis[i];
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".avisHtml");
-    // Création d’une balise dédiée à une pièce automobile
-    const avisElement = document.createElement("article");
-    // Création des balises 
-    const datesElement = document.createElement("p");
-    datesElement.innerText = article.Dates;
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = article.Nom;
-    const courseElement = document.createElement("p");
-    courseElement.innerText = article.Course;
-    const securiteElement = document.createElement("p");
-    securiteElement.innerText = article.Securite;
-    const balisageElement = document.createElement("p");
-    balisageElement.innerText = article.Balisage;
-    const interetsElement = document.createElement("p");
-    interetsElement.innerText = article.Interets;
-    const servicesElement = document.createElement("p");
-    servicesElement.innerText = article.Services;
-    const parcoursElement = document.createElement("p");
-    parcoursElement.innerText = article.Parcours;
-    const commentaireElement = document.createElement("p");
-    commentaireElement.innerText = article.Commentaire;
-    const etoileElement = document.createElement("p");
-    etoileElement.innerText = article.etoile;
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description;
+// for (let i = 0; i < avis.length; i++) {
+
+//     const article = avis[i];
+//     // Récupération de l'élément du DOM qui accueillera les fiches
+//     const sectionFiches = document.querySelector(".avisHtml");
+//     // Création d’une balise dédiée à une pièce automobile
+//     const avisElement = document.createElement("article");
+//     // Création des balises 
+//     const datesElement = document.createElement("p");
+//     datesElement.innerText = article.Dates;
+
+//     const nomElement = document.createElement("h2");
+//     nomElement.innerText = article.Nom;
+
+//     const courseElement = document.createElement("p");
+//     courseElement.innerText = article.Course;
+
+//     const securiteElement = document.createElement("p");
+//     securiteElement.innerText = article.Securite;
+
+//     const balisageElement = document.createElement("p");
+//     balisageElement.innerText = article.Balisage;
+
+//     const interetsElement = document.createElement("p");
+//     interetsElement.innerText = article.Interets;
+
+//     const servicesElement = document.createElement("p");
+//     servicesElement.innerText = article.Services;
+
+//     const parcoursElement = document.createElement("p");
+//     parcoursElement.innerText = article.Parcours;
+
+//     const commentaireElement = document.createElement("p");
+//     commentaireElement.innerText = article.Commentaire;
+
+//     const reponseElement = document.createElement("p");
+//     reponseElement.innerText = article.Nbreponse;
+
+//     const lireElement = document.createElement("p");
+//     lireElement.innerText = article.Lire;
 
     
-    // On rattache la balise article a la section Fiches
-    sectionFiches.appendChild(avisElement);
-    avisElement.appendChild(nomElement);
-    avisElement.appendChild(villeElement);
-    avisElement.appendChild(etoileElement);
-    avisElement.appendChild(descriptionElement);
- }
+
+    
+//     // On rattache la balise article a la section Fiches
+//     sectionFiches.appendChild(avisElement);
+//     avisElement.appendChild(datesElement);
+//     avisElement.appendChild(nomElement);
+//     avisElement.appendChild(courseElement);
+//     avisElement.appendChild(securiteElement);
+//     avisElement.appendChild(balisageElement);
+//     avisElement.appendChild(interetsElement);
+//     avisElement.appendChild(servicesElement);
+//     avisElement.appendChild(parcoursElement);
+//     avisElement.appendChild(commentaireElement);
+//     avisElement.appendChild(reponseElement);
+//     avisElement.appendChild(lireElement);
+//  }
