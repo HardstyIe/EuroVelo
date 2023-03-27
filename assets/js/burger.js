8d32c131fa34bcb233e2b66dbdb50e2415694e24
ASAPJulio <juliosarafyan@gmail.com>
2023-03-27 11:33:52 +0200

    sidebar

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,23 +1,35 @@
-document.querySelector('#open-menu').addEventListener('click', () => {
-  const aside = document.querySelector('aside')
-  const toggle = aside.classList.toggle('active')
-  const openMenu = document.querySelector('#open-menu')
-  console.log(toggle)
-  if (toggle) {
-    openMenu.src = '/assets/images/close.png'
-  } else {
-    openMenu.src = '/assets/images/menu-burger.png'
-  }
-})
+// document.querySelector('#open-menu').addEventListener('click', () => {
+//   const aside = document.querySelector('aside')
+//   const toggle = aside.classList.toggle('active')
+//   const openMenu = document.querySelector('#open-menu')
+//   console.log(toggle)
+//   if (toggle) {
+//     openMenu.src = '/assets/images/close.png'
+//   } else {
+//     openMenu.src = '/assets/images/menu-burger.png'
+//   }
+// })
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-const menuBurger = document.querySelector('.btn-burger');
+// const menuBurger = document.querySelector('ti-menu');
+// const sidebar = document.querySelector('sidebar');
+
+// menuBurger.addEventListener('click', () => {
+//   sidebar.classList.toggle('active');
+// });
+
+
+
+const burgerMenu = document.querySelector('.ti-menu');
 const sidebar = document.querySelector('.sidebar');
 
-menuBurger.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
+
+burgerMenu.addEventListener('click', function() {
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'flex';
+  } else {
+    sidebar.style.display = 'none';
+  }
+
 });
-
-
-
